40a920e7a56543fb408a9e79ec0bac9074a641d1
madunn5 <141796676+madunn5@users.noreply.github.com>
2024-05-23 13:04:06 -0700

    Admin updates to add categories from the posts in admin. Plus update to html code to properly display multiple categories.

--- blogging/admin.py
+++ blogging/admin.py
@@ -2,5 +2,30 @@ from django.contrib import admin
 
 from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryInline(admin.TabularInline):
+    model = Category.post.through
+    extra = 1
+    verbose_name = 'Category'
+    verbose_name_plural = 'Categories'
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_date', 'published_date')
+    search_fields = ('title', 'text')
+    list_filter = ('created_date', 'published_date')
+    inlines = [CategoryInline]
+
+    def get_categories(self, obj):
+        return obj.get_categories()
+    get_categories.short_description = 'Categories'
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+    exclude = ('posts',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
