328948291d2bcb630fbd7a02d642009a06088dd3
madunn5 <141796676+madunn5@users.noreply.github.com>
2024-06-04 20:52:14 -0700

    Update to allowed hosts.

--- mysite/heroku.py
+++ mysite/heroku.py
@@ -12,7 +12,7 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 SECRET_KEY = os.environ.get("SECRET_KEY")
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["afternoon-atoll-93236-9f699e921dfe.herokuapp.com"]
 
 MIDDLEWARE = (
     "whitenoise.middleware.WhiteNoiseMiddleware",
